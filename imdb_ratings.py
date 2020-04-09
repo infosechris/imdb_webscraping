26df19f79ca6eb36a3756e1b1e14464d4a9bb826
Chris <45928077+infosechris@users.noreply.github.com>
2020-04-09 18:50:42 -0400

    Update imdb_ratings.py

--- imdb_ratings.py
+++ imdb_ratings.py
@@ -11,6 +11,17 @@ url4 = url1 + url2 + url3
 html = requests.get(url4).text
 soup = BeautifulSoup (html, 'html.parser')
 
+t1 = soup.find('h3', attrs={'itemprop': 'name'})
+titleyear = t1.text.split()
+year = titleyear[-1]
+del titleyear[-1]
+
+title = ""
+for i in range(0, len(titleyear)):
+    title = title + " " + titleyear[i]
+
+print ('Title: ' + title + '\nYear(s): ' + year)
+
 t_seasons = []
 for option in soup.find_all('option'):
     stemp = option.text.split()
@@ -23,6 +34,8 @@ for i in range(0, len(c_seasons)):
     k = int(c_seasons[i][0])
     if k <= 1000:
         seasons.append(c_seasons[i][0])
+
+print ('Total Seasons:', seasons[-1], '\n')
 
 for season in range(1, len(seasons)+1):
     url4 = url1 + url2 + str(season)
@@ -38,6 +51,8 @@ for season in range(1, len(seasons)+1):
         counter += 1
         eps.append(counter)    
 
-    print ("Current Season:", season)
+    print ("Season:", season)
     print ("Episodes: ", eps)
     print ("Ratings: ", rating, "\n")
+
+    
