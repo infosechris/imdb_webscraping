8afd720e807f996ee897af2ab5a1de9630b441b7
Chris <45928077+infosechris@users.noreply.github.com>
2020-04-10 21:04:33 -0400

    Update imdb_ratings.py

--- imdb_ratings.py
+++ imdb_ratings.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import csv
+import sys
 
 season = 1
 perEP = 0
@@ -13,14 +18,19 @@ soup = BeautifulSoup (html, 'html.parser')
 
 t1 = soup.find('h3', attrs={'itemprop': 'name'})
 titleyear = t1.text.split()
-year = titleyear[-1]
-del titleyear[-1]
+if titleyear[-1] == ')':
+    year = titleyear[-2] + titleyear[-1]
+    del titleyear[-1]
+    del titleyear[-1]
+else:
+    year = titleyear[-1]
+    del titleyear[-1]
 
 title = ""
 for i in range(0, len(titleyear)):
     title = title + " " + titleyear[i]
 
-print ('Title: ' + title + '\nYear(s): ' + year)
+print ('Title: ' + title + '\nYears: ' + year)
 
 t_seasons = []
 for option in soup.find_all('option'):
@@ -31,12 +41,21 @@ c_seasons = [x for x in t_seasons if x]
 
 seasons = []
 for i in range(0, len(c_seasons)):
-    k = int(c_seasons[i][0])
-    if k <= 1000:
+    if c_seasons[i][0].isnumeric():
+        k = int(c_seasons[i][0])
+        if k <= 1000:
+            seasons.append(c_seasons[i][0])
+    else:
         seasons.append(c_seasons[i][0])
 
-print ('Total Seasons:', seasons[-1], '\n')
+print ('Total Seasons:', len(seasons), '\n')
 
+#array = np.array([])
+#f = open(sys.argv[1], 'wt')
+#writer = csv.writer(f)
+
+csvR = []
+epsMax = 0
 for season in range(1, len(seasons)+1):
     url4 = url1 + url2 + str(season)
     html = requests.get(url4).text
@@ -49,10 +68,35 @@ for season in range(1, len(seasons)+1):
         rtemp = div.find('span', class_='ipl-rating-star__rating')
         rating.append(rtemp.text)
         counter += 1
-        eps.append(counter)    
+        eps.append(counter)
+        
+        if epsMax < counter:
+            epsMax = counter   
 
-    print ("Season:", season)
-    print ("Episodes: ", eps)
-    print ("Ratings: ", rating, "\n")
+    csvR.append([season] + rating)
 
-    
+tempE = ['']
+i=1
+while i <= epsMax:
+    tempE.append(i)
+    i+=1
+
+csvE = []
+csvE.append(tempE)
+
+print(csvR)
+print(csvE)
+
+with open("out.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(csvE)
+    writer.writerows(csvR)
+
+    #print ("Season:", season, 'of', len(seasons))
+    #print ("Episodes:", eps)
+    #print ("Ratings:", rating, "\n")
+
+
+    #rating.insert(len(rating),'n')
+    #tempA = np.array(rating)
+    #array = np.append(array, tempA, axis=0)
